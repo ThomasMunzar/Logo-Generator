c839984e61950ea17bf192e73796ace47cefabd5
Thomas Munzar <tmunzar92@gmail.com>
2023-07-24 15:23:15 -0700

    added files/some pseudo code

---
+++ index.js
@@ -0,0 +1,18 @@
+// 0. import statement of packages
+// const shape = require('./lib/shapes.js');
+// const Triangle = shape.Triangle;
+// const Circle = shape.Circle;
+// const Square = shape.Square
+// // OR 
+// const {Triangle} = require('./lib/shapes.js')
+// const {Circle} = require('./lib/shapes.js')
+// const {Square} = require('./lib/shapes.js')
+// OR
+const {Triangle,Square,Circle} = require('./lib/shapes.js')
+
+
+//1. Prompt user for text (can only be 3 characters), shape, color of text, color of shape. 
+
+//2. Create svg file
+    //2.1 
+    //2.9 use writeFile to create .SVG file
